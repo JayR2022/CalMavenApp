0a13cc990711bb3d4e94debe4794a7223de6e32a
Jay.R <jwatson2022@protonmail.com>
2022-04-25 15:30:40 +0100

    CAL-18 #comment initial commit

---
+++ src/test/java/ui/UiTest.java
@@ -0,0 +1,33 @@
+package ui;
+
+import org.junit.Test;
+
+public class UiTest {
+	
+	@Test
+	public void runTest() {
+		
+	}
+	
+	@Test
+	public void getOperationsTest() {
+		
+	}
+	
+	@Test
+	public void getFirstNumTest() {
+		
+	}
+	
+	@Test
+	public void getSecondNumTest() {
+		
+	}
+	
+	@Test
+	public void getResultsTest() {
+		
+	}
+	
+
+}
